f6f053ea8682453bc307334e5aa74854f258c97b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-03-06 11:22:06 +0000

    Bundle masto.js

---
+++ src/masto/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import {
+	MastoDeserializeError,
+	MastoHttpError,
+	MastoInvalidArgumentError,
+	MastoTimeoutError,
+	MastoUnexpectedError,
+	MastoWebSocketError,
+	createOAuthAPIClient,
+	createRestAPIClient,
+	createStreamingAPIClient,
+	mastodon
+} from "masto";
+
+export {
+	MastoDeserializeError,
+	MastoHttpError,
+	MastoInvalidArgumentError,
+	MastoTimeoutError,
+	MastoUnexpectedError,
+	MastoWebSocketError,
+	createOAuthAPIClient,
+	createRestAPIClient,
+	createStreamingAPIClient,
+	mastodon
+};
